96573e15a3438945d8a5dd83f148b525d12d6826
Jeong Pak <dannypak716@gmail.com>
2021-12-02 13:57:13 -0500

    can post new posts to a user. finished

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -12,7 +12,8 @@ async function validateUserId(req, res, next) {
   try {
     const user = await User.getById(req.params.id);
     if (!user) {
-      res.status(404).json({
+      next({
+        status: 404,
         message: 'user not found'
       })
     } else {
